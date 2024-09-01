268ef7228f1ddd297d618226cbf9dbb3f6c449d3
Pablo Lambert <pablomartin.lambert@gmail.com>
2024-09-01 00:04:41 -0400

    prueba deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import ghPages from 'vite-plugin-gh-pages'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [ghPages()],
-    base: '/celvaprod-three-js/' // Reemplaza "tu-repositorio" por el nombre de tu repo
-})
+    base: '/celvaprod-three-js/',
+});
